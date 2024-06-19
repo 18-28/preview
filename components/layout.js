e7869b4217e72e0294599fddbee67cd17c1d8a5e
zacharysutton-bg <108938149+zacharysutton-bg@users.noreply.github.com>
2024-06-19 14:49:22 +0100

    update to test vercel

--- components/layout.js
+++ components/layout.js
@@ -24,6 +24,7 @@ const Layout = (props) => {
                 {props.children.props.page.data.story.content.body.map(component => {
                     return(
                         <div>
+                            VERCEL CHANGES?
                             {switchHandle(component)}
                         </div>
                     )
