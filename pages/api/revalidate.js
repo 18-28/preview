51184b7ef190c1678b3d64697600437171578a68
Zachary Sutton <zachary-sutton@outlook.com>
2024-08-28 13:10:32 +0100

    fixed bug in reval for webhook

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -3,7 +3,7 @@ export default async function revalidate(req, res){
     console.log(req.body.text)
 
     try {
-        await res.revalidate(req.body.full_slug)
+        await res.revalidate(`/${req.body.full_slug}`)
         return res.status(200).json({ message: `Revalidated: ${req.body.full_slug}` })
     } catch (error) {
         return res.status(500).json({ error: error.message })
