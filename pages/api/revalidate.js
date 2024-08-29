51e00d5ebfc7a72b147b0352519176016452340c
Zachary Sutton <zachary-sutton@outlook.com>
2024-08-29 10:40:06 +0100

    fix home slug not reval

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -3,8 +3,12 @@ export default async function revalidate(req, res){
     console.log(req.body.text)
     console.log(req.body.full_slug)
 
+    const revalidationSlug = req.body.full_slug === 'home' ? '/' : `/${req.body.full_slug}`
+
+    console.log(revalidationSlug)
+
     try {
-        await res.revalidate(`/${req.body.full_slug}`)
+        await res.revalidate(revalidationSlug)
         return res.status(200).json({ message: `Revalidated: ${req.body.full_slug}` })
     } catch (error) {
         return res.status(500).json({ error: error.message })
