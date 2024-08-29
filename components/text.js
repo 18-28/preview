eeceed1b0a87380db6d8a512e08f7b7d6009c77c
Zachary Sutton <zachary-sutton@outlook.com>
2024-08-29 13:53:43 +0100

    added some reval logic

--- components/text.js
+++ components/text.js
@@ -1,5 +1,22 @@
+import { useEffect } from "react"
+import StoryblokClient from 'storyblok-js-client'
+
 const Text = (props) => {
-    console.log(props)
+    useEffect(() => {       
+          const data = async () => {
+
+            const Storyblok = new StoryblokClient({
+                accessToken: 'TZNKj6AGu78yS8UIHxicTAtt'
+              })
+
+            const stories = await Storyblok.get(`cdn/stories`, {
+                version: 'published'
+              })
+            console.log(stories)
+        }
+        data()
+    })
+
     return(
         <>
         HARD CODE
--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,16 +1,33 @@
+import StoryblokClient from 'storyblok-js-client'
+
+
 export default async function revalidate(req, res){
 
-    console.log(req.body.text)
-    console.log(req.body.full_slug)
+    //get storyblok published stories
+    //filter stories array with body.full_slug
+    //if is in array call res.revalidate
+    //if isn't in array, redeploy the branch
 
+    //create another endpoint for unpublishing
+
+    const Storyblok = new StoryblokClient({
+        accessToken: 'TZNKj6AGu78yS8UIHxicTAtt'
+      })
+    
+      const publishedStories = await Storyblok.get(`cdn/stories`, {
+        version: 'published'
+      })
+
+    const isPublished = publishedStories.data.stories.findIndex(story => story.full_slug === req.body.full_slug)
     const revalidationSlug = req.body.full_slug === 'home' ? '/' : `/${req.body.full_slug}`
 
-    console.log(revalidationSlug)
-
-    try {
-        await res.revalidate(revalidationSlug)
-        return res.status(200).json({ message: `Revalidated: ${req.body.full_slug}` })
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
+    if(isPublished != -1){
+      try {
+          await res.revalidate(revalidationSlug)
+          return res.status(200).json({ message: `Revalidated: ${req.body.full_slug}` })
+      } catch (error) {
+          return res.status(500).json({ error: error.message })
+      }
     }
 }
+
